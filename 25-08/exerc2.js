a6d628d895bf26f26c2fbf0770fdba142902ed53
Winnie <wssant13@gmail.com>
2025-08-25 23:07:52 -0300

    quito commit

--- 25-08/exerc2.js
+++ 25-08/exerc2.js
@@ -1,2 +1,58 @@
 const leia = require('readline-sync')
 
+let opcao, saldo
+
+saldo = 1000.00
+
+console.log("1 - Saldo, 2 - Saque, 3 - Deposito" )
+
+opcao = leia.questionFloat("Digite a opcao desejada: ")
+
+switch(opcao){
+
+case 1:
+    saldo = new Intl.NumberFormat('pt-BR', {
+        style: "currency",
+        currency: "BRL"
+    }).format(saldo)
+
+    console.log("Saldo na conta: " + saldo);
+    break
+case 2:
+    let valorSaque = leia.questionFloat("Digite o valor do saque: ");
+
+    if(valorSaque > saldo){
+        console.log("Saldo Insuficiente!")
+        break
+    }
+
+    saldo -= valorSaque
+    saldo = new Intl.NumberFormat('pt-BR', {
+        style: "currency",
+        currency: "BRL"
+    }).format(saldo)
+
+    console.log("Novo Saldo: " + saldo)
+    break
+case 3:
+    let valorDeposito = leia.questionFloat("Digite o valor do deposito: ");
+
+    if(valorDeposito > saldo){
+        console.log("Saldo Insuficiente!")
+        break
+    }
+    
+    saldo += valorDeposito
+    saldo = new Intl.NumberFormat('pt-BR', {
+        style: "currency",
+        currency: "BRL"
+    }).format(saldo) 
+
+    console.log("Novo Saldo: " + saldo)
+    break
+default:
+    console.log("Opcao invalida!")
+}
+
+
+
